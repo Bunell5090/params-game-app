69903cb2957eab9d4f4fa4d2b8ceecea27942ac7
Bunell5090 <Bunell5090.com>
2022-03-10 20:45:23 -0600

    Updated Routes & created request.http file

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "/url_segment_params/:variable" => "params_examples#url_segment"
   get "/first_letter_params", controller: "games", action: "first_letter_params"
   get "/number_game" => "games#number_game"
+  post "/body_params" => "params_examples#body_params"
 end
