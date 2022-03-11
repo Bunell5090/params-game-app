587b12f21e23012c3e834f36c0acb018d00d5744
Bunell5090 <Bunell5090.com>
2022-03-10 20:53:04 -0600

    Updated Params controller

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -6,10 +6,22 @@ class ParamsController < ApplicationController
 
   
 
-    def url_segment
-      input_value = params["variable"]
-      render json: {message: "The url segment is #{input_value}"}
-    end
+  def url_segment
+    input_value = params["variable"]
+    render json: {message: "The url segment is #{input_value}"}
+  end
+
+  def body_segment
+    input_email = params["user_email"]
+    input_user = params["username"]
+    input_ssn = params["user_ssn"]
+    input_pass = params["password"]
+
+    if input_user == "hugh" && input_pass == "swordfish"
+      render json: {message: "Valid credentials"}
+    else
+      render json: {message: "Invalid credentials"}    
+  end
 
    
 
